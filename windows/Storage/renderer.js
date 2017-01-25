d2f18603d8d2bce8d1f68d0852d310f0231e489b
Lorenzo Setale <lorenzo@setale.me>
2017-01-25 19:31:09 +0100

    shows the list of pinned objects

--- windows/Storage/renderer.js
+++ windows/Storage/renderer.js
@@ -69,11 +69,27 @@ function refreshStorageList(){
 
         // From Object to Array
         let pins = []
-        for(let key in pinsObj) {
-          pins.push(pinsObj[key])
+        for(let hash in pinsObj) {
+          var obj = pinsObj[hash]
+          obj.hash = obj.hash || hash
+          pins.push(obj)
         }
 
         pins = pins.filter( (el) => el.Type !== "indirect")
+        // ToDo: Should we use React here? :) Just doing things quickly for now
+
+        let tableBody = document.getElementById('obj-list')
+        // Clean everything
+        tableBody.innerHTML = ""
+        pins.forEach((el)=>{
+          let row = document.createElement("tr")
+          let hashColumn = document.createElement("td")
+          let sizeColumn = document.createElement("td")
+          hashColumn.innerText = el.hash
+          row.appendChild(hashColumn)
+          row.appendChild(sizeColumn)
+          tableBody.appendChild(row)
+        })
         return success(pins)
       }, failure)
   })
