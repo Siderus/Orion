046feee69ed196eb9830ebae3ff5db3cc2518a44
Lorenzo Setale <lorenzo@setale.me>
2017-01-28 14:27:04 +0100

    shows the app only when ready

--- main.js
+++ main.js
@@ -36,7 +36,9 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (app.mainWindow) {
-    app.mainWindow.show()
+    app.mainWindow.once('ready-to-show', () => {
+      app.mainWindow.show()
+    })
   }else{
     app.mainWindow = StorageWindow.create(app)
   }
