d4c91cab18dd72f31a6a17bd176c58c7a876486e
Lorenzo Setale <lorenzo@setale.me>
2017-02-01 23:15:09 +0100

    re-introuduce the copy-paste functuonality

--- app/menu.js
+++ app/menu.js
@@ -23,6 +23,35 @@ let template = [{
     }
   }]
 }, {
+  label: 'Edit',
+  submenu: [{
+    label: 'Undo',
+    accelerator: 'CmdOrCtrl+Z',
+    role: 'undo'
+  }, {
+    label: 'Redo',
+    accelerator: 'Shift+CmdOrCtrl+Z',
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Cut',
+    accelerator: 'CmdOrCtrl+X',
+    role: 'cut'
+  }, {
+    label: 'Copy',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }, {
+    label: 'Paste',
+    accelerator: 'CmdOrCtrl+V',
+    role: 'paste'
+  }, {
+    label: 'Select All',
+    accelerator: 'CmdOrCtrl+A',
+    role: 'selectall'
+  }]
+}, {
   label: 'View',
   submenu: [{
     label: 'Reload',
