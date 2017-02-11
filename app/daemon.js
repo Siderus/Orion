294fa40f1ed431ac837ad3b252e462fe48d17fef
Lorenzo Setale <lorenzo@setale.me>
2017-02-11 11:36:12 +0100

    allows custom binary path for IPFS

--- app/daemon.js
+++ app/daemon.js
@@ -2,12 +2,22 @@
 
 import { spawn, execSync } from "child_process"
 
+import Settings from 'electron-settings'
+
+/**
+ * getPathIPFSBinary will return the IPFS default path
+ */
+export function getPathIPFSBinary() {
+  return Settings.getSync("daemon.pathIPFSBinary") || "/usr/local/bin/ipfs"
+}
+
 /**
  * startIPFSCommand will start IPFS go daemon, if installed.
  * return child process with IPFS daemon
  */
 export function startIPFSCommand() {
-  const ipfs_process = spawn("/usr/local/bin/ipfs", ["daemon"])
+  const binaryPath = getPathIPFSBinary()
+  const ipfs_process = spawn(binaryPath, ["daemon"])
 
   ipfs_process.stdout.on("data", (data) => console.log(`IPFS: ${data}`))
   ipfs_process.stderr.on("data", (data) => console.log(`IPFS Error: ${data}`))
@@ -20,7 +30,8 @@ export function startIPFSCommand() {
  * Returns the multiAddr usable to connect to the local dameon via API
  */
 export function getMultiAddrIPFSDaemon(){
-  let multiAddr = execSync('/usr/local/bin/ipfs config Addresses.API')
+  const binaryPath = getPathIPFSBinary()
+  let multiAddr = execSync(`${binaryPath} config Addresses.API`)
   return `${multiAddr}`
 }
 
@@ -29,6 +40,7 @@ export function getMultiAddrIPFSDaemon(){
  * It restores it to /ip4/127.0.0.1/tcp/5001
  */
 export function setMultiAddrIPFSDaemon(){
-  let multiAddr = execSync('/usr/local/bin/ipfs config Addresses.API /ip4/127.0.0.1/tcp/5001')
+  const binaryPath = getPathIPFSBinary()
+  let multiAddr = execSync(`${binaryPath} config Addresses.API /ip4/127.0.0.1/tcp/5001`)
   return `${multiAddr}`
 }
