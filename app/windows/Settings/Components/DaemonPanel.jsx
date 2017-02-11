1b693407ac6e11f3bbe68c801bcb9f3b8699f473
Lorenzo Setale <lorenzo@setale.me>
2017-02-11 11:38:59 +0100

    adds skeleton for the daemon panel

--- app/windows/Settings/Components/DaemonPanel.jsx
+++ app/windows/Settings/Components/DaemonPanel.jsx
@@ -1,7 +1,10 @@
+import Settings from 'electron-settings'
 import React from "react"
 import { observer } from "mobx-react"
 
-import { Pane } from "react-photonkit"
+import { getMultiAddrIPFSDaemon, getPathIPFSBinary } from '../../../daemon'
+
+import { Pane, Input, CheckBox } from "react-photonkit"
 
 /**
  * Daemon Panel
@@ -9,12 +12,45 @@ import { Pane } from "react-photonkit"
 
 @observer
 class DaemonPanel extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.settings = {}
+  }
+
+  componentDidMount() {
+    Settings.get('daemon').then( val => this.settings = val )
+  }
+
+  _handelOnSubit(event){
+
+  }
+
   render() {
     if(this.props.navigationStore.selected != 2) return null
+    let data = this.settings || {}
 
     return (
-      <Pane>
-        <h1>Daemon</h1>
+      <Pane className="settings">
+        <form onSubmit={this._handelOnSubit.bind(this)}>
+
+          <Input
+            label="API multiaddress to use:"
+            type="text"
+            value={data.pathIPFSBinary || getMultiAddrIPFSDaemon()}
+            placeholder="Hey girl..."/>
+
+          <Input
+            label="Path of the IPFS binary"
+            type="text"
+            value={data.pathIPFSBinary || getPathIPFSBinary()}
+            placeholder="/usr/local/bin/ipfs"/>
+
+          <CheckBox label="Try to start IPFS Daemon when the app opens" />
+
+          <label>Restart is required</label>
+
+        </form>
       </Pane>
     )
   }
