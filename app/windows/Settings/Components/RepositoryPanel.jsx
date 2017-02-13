b37bbac0ddd528e5ddf9c361865d575d263ec8ef
Lorenzo Setale <lorenzo@setale.me>
2017-02-13 10:54:20 +0100

    connects the garbage collector button to the API

--- app/windows/Settings/Components/RepositoryPanel.jsx
+++ app/windows/Settings/Components/RepositoryPanel.jsx
@@ -3,18 +3,33 @@ import { observer } from "mobx-react"
 
 import { Pane, Input, TextArea, Button } from "react-photonkit"
 
+import { runGarbageCollector } from "../../../api"
+
 /**
  * Repository Panel
  */
 
 @observer
 class RepositoryPanel extends React.Component {
+
+  /** Perform garbage collector and reload the view when done */
+  _handleButtonGarbageCollectorClick(event){
+    runGarbageCollector()
+    .then(this.props.informationStore.loadData)
+    .then(this.forceUpdate)
+  }
+
   _handelOnSubit(event){
 
   }
 
   render() {
     if(this.props.navigationStore.selected != 0) return null
+    if(!this.props.informationStore) return null
+    if(!this.props.informationStore.loaded) return null
+
+    const data = this.props.informationStore.repoStats
+    let repoSize = `${data.RepoSize.value} ${data.RepoSize.unit}`
 
     return (
       <Pane className="settings">
@@ -23,15 +38,20 @@ class RepositoryPanel extends React.Component {
           <Input
             label="Path of the Repository"
             type="text"
-            value="/123"
-            placeholder="Hey girl..." readOnly/>
+            defaultValue={data.RepoPath || "$HOME... maybe? Whatever, it's 2009!"}
+            placeholder="Hey girl..."/>
 
           <Input
             label="Repository Size:"
             type="text"
+            value={repoSize}
             placeholder="Hey girl..." readOnly/>
 
-            <Button ptSize="large" glyph="trash" text="Run Garbage Collector" />
+            <Button
+              onClick={this._handleButtonGarbageCollectorClick.bind(this)}
+              ptSize="large"
+              glyph="trash"
+              text="Run Garbage Collector" />
 
         </form>
       </Pane>
