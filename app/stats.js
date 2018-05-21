75c981aa9f4616cf5fe6beb30d0b2c322cb25c14
Lorenzo Setale <lorenzo@setale.me>
2018-05-21 20:46:55 +0200

    adds useful info to event for debugging and flow

--- app/stats.js
+++ app/stats.js
@@ -3,6 +3,7 @@ import Settings from 'electron-settings'
 import uuidv4 from 'uuid/v4'
 import pjson from '../package.json'
 import publicIp from 'public-ip'
+import {release, platform} from 'os'
 
 const SettingsUserIDKey = 'statsUserID'
 
@@ -16,39 +17,49 @@ let UserID = Settings.getSync(SettingsUserIDKey)
  * setUpUser will configure the client in order to track errors, events and
  * follow users when debugging process.
  */
-export function setUpUser () {
-  return publicIp.v4().then(ipAddress => {
-    if (!Settings.getSync(SettingsUserIDKey)) {
-      UserID = uuidv4()
-      Settings.setSync(SettingsUserIDKey, UserID)
-    }
+export function setUpUser (ipAddress) {
+  if (!Settings.getSync(SettingsUserIDKey)) {
+    UserID = uuidv4()
+    Settings.setSync(SettingsUserIDKey, UserID)
+  }
 
-    UserID = Settings.getSync(SettingsUserIDKey)
-    client.people.set(UserID, {
-      version: `${pjson.version}`
-    }, {
-      $ip: ipAddress
-    })
+  UserID = Settings.getSync(SettingsUserIDKey)
+  client.people.set(UserID, {
+    $first_name: UserID,
+    version: `${pjson.version}`,
+    os_release: `${release()}`,
+    os_platform: `${platform()}`,
+  }, {
+    $ip: ipAddress
   })
 }
 
 /*
- * trackEvent will send a new event with specific data
+ * trackEvent will send a new event with specific data. It will get the user
+ * IP address, add/updates the platform, version, release to the profile and 
+ * then track the event.
+ * 
+ * It returns a Promise
  */
 export function trackEvent (eventName, data) {
-  return publicIp.v4().then(ipAddress => {
-    if (!UserID) {
-      setUpUser()
-    }
+  return new Promise((resolve, reject) => {
+    return publicIp.v4().then(ipAddress => {
+      setUpUser(ipAddress) 
+  
+      if (!data) {
+        data = {}
+      }
+  
+      data.distinct_id = UserID
+      data.version = `${pjson.version}`
+      client.track(eventName, data, (err) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve()
+      })
 
-    if (!data) {
-      data = {}
-    }
-
-    data.distinct_id = UserID
-    data.version = `${pjson.version}`
-    data.$ip = ipAddress
-    console.log("Event", eventName, data)
-    client.track(eventName, data)
-  })
+    })
+  }) 
 }
+
