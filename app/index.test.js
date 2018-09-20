27a59aaafb5d89ec9be329f1bf883321ae397aa5
Lorenzo Setale <lorenzo@setale.me>
2018-09-20 15:16:05 +0200

    adds a test for clear activities that are interrupted

--- app/index.test.js
+++ app/index.test.js
@@ -23,7 +23,7 @@ jest.mock('electron', () => ({
 
 describe('index.js', () => {
   describe('clear-activities', () => {
-    it('should only clear unfinished activities', () => {
+    it('should only clear finished activities', () => {
       // arrange
       const activitiesById = [1, 2, 3]
       const activities = {
@@ -51,6 +51,42 @@ describe('index.js', () => {
         '1': {
           uuid: 1,
           finished: false,
+          foo: 'b'
+        }
+      })
+    })
+    it('should  clear interrupted activities', () => {
+      // arrange
+      const activitiesById = [1, 2, 3]
+      const activities = {
+        '1': {
+          uuid: 1,
+          finished: false,
+          interrupted: false,
+          foo: 'b'
+        },
+        '2': {
+          uuid: 2,
+          finished: true,
+          interrupted: false,
+          foo: 'a'
+        },
+        '3': {
+          uuid: 3,
+          finished: false,
+          interrupted: true,
+          foo: 'r'
+        }
+      }
+      // act
+      const result = AppIndex.filterUnfinishedActivities(activitiesById, activities)
+      // assert
+      expect(result.activitiesById).toEqual([1])
+      expect(result.activities).toEqual({
+        '1': {
+          uuid: 1,
+          finished: false,
+          interrupted: false,
           foo: 'b'
         }
       })
